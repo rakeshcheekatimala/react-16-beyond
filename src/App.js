8f0c030d99d0232f987821ea0a38bb1a56f5afe5
rakesh.cheekatimala <rakesh.cheekatimala@singtel.com>
2020-06-24 21:01:11 +0800

    chatpanel integration in the App Component and button click to add a new chatmessage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { ChatPanel } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    points: 10,
+    chatList: ["Hey", "Hello", "Hi"],
+  };
+
+  // this is life cycle method that would be called before render method and overrides the state
+  static getDerivedStateFromProps(props, state) {
+    return {
+      points: 1000,
+    };
+  }
+
+  _handleAddChat = () => {
+    let chatList = [...this.state.chatList];
+    chatList.push("Hello on " + new Date().toLocaleString());
+    this.setState({
+      chatList,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            src="https://www.educative.io/udata/WaYwm2ebRW7/Screen%20Shot%202019-08-21%20at%205.24.29%20PM.png"
+            className="App-logo"
+            alt="logo"
+          />
+          <p>You've scored {this.state.points} points.</p>
+        </header>
+        <section className="App-chat">
+          <p className="chat-header">
+            Football Chat{" "}
+            <button className="chat-btn" onClick={this._handleAddChat}>
+              Add Chat
+            </button>
+          </p>
+          <ul className="chat-thread" ref={this.chatThreadRef}>
+            <ChatPanel chatList={this.state.chatList} />
+          </ul>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
